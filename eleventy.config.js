c0e02bcb992246f40f471c49b5395624057f2e2d
Arvind Suresh <arvind.suresh@smartstaff.co.in>
2025-01-28 00:17:56 +0530

    fix: center images

--- eleventy.config.js
+++ eleventy.config.js
@@ -31,7 +31,7 @@ export default async function (eleventyConfig) {
        <div class="block md:hidden relative bg-brand-900 rounded-md h-[40vh] md:h-[50vh] overflow-hidden group duration-[300ms] taos:translate-y-[200px] taos:opacity-0" data-taos-offset="${
          offset * 25
        }">
-        <a class="block relative w-full h-full bg-cover" href="${url}" style="background-image: url('${imagePath}');">
+        <a class="block relative w-full h-full bg-cover bg-center" href="${url}" style="background-image: url('${imagePath}');">
           <div class="absolute h-12 bottom-0 left-0 right-0 flex items-center justify-center p-4 bg-brand-300 text-white uppercase font-bold text-center">
             ${title}
           </div>
